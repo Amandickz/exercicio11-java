a58fe362f8372f9b162c1e714c8dd178abd4e587
Amandickz <amandickz@gmail.com>
2024-12-12 09:31:21 -0300

    Envio dos primeiros arquivos

---
+++ src/classes/Company.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Company extends TaxPayer{
+
+    private int numberOfEmployees;
+
+    public Company(String name, double anualIncome, int numberOfEmployees) {
+        super(name, anualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+}
